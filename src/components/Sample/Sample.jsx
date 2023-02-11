88f8e55e666dbe181f9df8114ae6b77567134446
Ilia Sharin <95469110+xxl3ko@users.noreply.github.com>
2023-02-11 09:35:01 +0400

    02-11 morning

--- src/components/Sample/Sample.jsx
+++ src/components/Sample/Sample.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
-const Sample = () => {
-    axios.get('http://127.0.0.1:8000/api/sample').then(response => {
-        console.log()
-    })
 
-    return <div>Oleg</div>
+const Sample = () => {
+    const [ura, setUra] = useState([])
+
+    axios
+        .get('http://127.0.0.1:8000/api/sample')
+        .then(data => {
+            setUra(data.data);
+        })
+
+    return (
+        <div>
+            {ura.map(sample => {
+                return (
+                    <div>
+                        {sample.title}
+                    </div>
+                )
+            })
+            }
+        </div>
+    )
 }
 
 export default Sample
